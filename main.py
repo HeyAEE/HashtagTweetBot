3960747dff6fd125159fe270381107137f2bd9b5
Adam <adotengel@gmail.com>
2015-12-23 13:10:56 -0500

    Work in progress. Script can now pull comments from the /r/baseball subreddit. Also has debugging statements in the form of print() checkpoints.

--- main.py
+++ main.py
@@ -14,23 +14,36 @@ Progress log:
 
 try:
     import praw as p
+    print('Successfully imported praw.')
 except ImportError:
     print('Uh oh! You don\'t have praw installed to access the reddit API. Install praw using the following command:\n\npip install praw\n\nand try again.')
         
 from sys import exit
+
+from pprint import PrettyPrinter
+printer = PrettyPrinter()
         
 def main():
     redditor = p.Reddit(user_agent='Hashtagger')
-    print('test')
+    print('Successfully created reddit UA.')
     login = []
     f = open('private/login_info','r')
     [login.append(str(line).rstrip()) for line in f]
+    f.close()
     try:
-        redditor.login(login[0],login[1])
-        print(redditor.user())
-    except praw.errors.InvalidUserPass:
-        print("Wrong username. Try again!")
-        sys.exit(1)
+        redditor.login(login[0],login[1], disable_warning=True)
+        print(redditor.user)
+        print('So far, so good!')
+    except p.errors.InvalidUserPass:
+        print("Wrong username or password. Try again!")
+        exit(1)
+    # So far, I have created a reddit user agent and logged in, such that I can now start listening to the reddit streams. Next step: listen to the streams so I can find Twitter linked comments.
+    comments = redditor.get_comments("baseball", limit=20)
+    print("pulled comments")
+    print("comments:\n")
+    comments_to_analyze = []
+    [comments_to_analyze.append(comment.body_html) for comment in comments]
+    [printer.pprint(comment) for comment in comments_to_analyze]
         
 if __name__=="__main__":
     main()
